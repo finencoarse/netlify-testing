2b51896699e44f898d5670a452c85102c1f5fb6a
finencoarse <154966371+finencoarse@users.noreply.github.com>
2026-01-20 14:55:18 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { ViewState, Language, UserProfile } from '../types';
-import { translations } from '../translations';
+import { ViewState, Language, UserProfile } from '@/types';
+import { translations } from '@/translations';
 
 interface HeaderProps {
   setView: (view: ViewState) => void;
